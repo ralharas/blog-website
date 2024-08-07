8fb9d9a44b379f50f11da2d0f190e499d7370fd1
Rawad Alharastani <ralharas@uwo.ca>
2024-08-06 23:11:35 -0400

    Sign in and Sign up layout and create new page functionality

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
+let posts = [];
+
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { posts });
 })
 
 router.get("/explore", (req, res) => {
@@ -14,8 +16,35 @@ router.get("/new-post", (req, res) => {
     res.render("new-post");
 })
 
+
+router.post("/new-post", (req, res) => {
+    const { title, description, content } = req.body;
+    const newPost = {
+        id: posts.length + 1,
+        title,
+        description,
+        content
+    };
+    posts.push(newPost);
+    res.redirect(`/post/${newPost.id}`);
+});
+
+router.get("/post/:id", (req, res) => {
+    const post = posts.find(p => p.id == req.params.id);
+    if (post) {
+        res.render("post", { post });
+    } else {
+        res.status(404).send("Post not found");
+    }
+});
+
+
+
 router.get("/signin", (req, res) => {
     res.render("signin");
 })
 
+router.get("/signup", (req, res) => {
+    res.render("signup");
+})
 export default router;
