6802eb2c5214700a3c0a2515dadf09e2920856df
Rawad Alharastani <ralharas@uwo.ca>
2024-08-07 00:10:47 -0400

    Quick update to previous push

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import bodyParser from "body-parser";
-import indexRouter from "./routes/index.js"; 
+import bodyParser from 'body-parser';
+import indexRouter from './routes/index.js';
 
 const app = express();
 const port = 3000;
 
-
 app.set('view engine', 'ejs');
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'assets'))); 
 
-
-app.use('/', indexRouter); 
-
+app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,33 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const router = express.Router();
 
 let posts = [];
+let postIdCounter = 1;
+const images = ['beach.jpg', 'img1.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg'];
 
-router.get("/", (req, res) => {
-    res.render("index", { posts });
-})
+router.get('/', (req, res) => {
+    res.render('index', { posts, images });
+});
 
 router.get("/explore", (req, res) => {
     res.render("explore");
-})
+});
 
 router.get("/new-post", (req, res) => {
     res.render("new-post");
-})
-
+});
 
 router.post("/new-post", (req, res) => {
     const { title, description, content } = req.body;
     const newPost = {
-        id: posts.length + 1,
+        id: postIdCounter++,
         title,
         description,
-        content
+        content,
+        image: images[Math.floor(Math.random() * images.length)]
     };
     posts.push(newPost);
     res.redirect(`/post/${newPost.id}`);
@@ -38,13 +42,12 @@ router.get("/post/:id", (req, res) => {
     }
 });
 
-
-
 router.get("/signin", (req, res) => {
     res.render("signin");
-})
+});
 
 router.get("/signup", (req, res) => {
     res.render("signup");
-})
+});
+
 export default router;
